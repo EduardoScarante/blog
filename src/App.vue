182a80a99a4bd5a3d578bbfafe950d8bf8beb07a
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-05-17 15:14:30 -0300

    remove lógica duplicada, acrescenta links e troca informações com o RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
+import '@/assets/main.css'
 
 export default {
   data() {
     return {
-      formData: {},
       posts: [
         {
           title: 'Primeiro Post',
@@ -32,49 +32,12 @@ export default {
           content: 'Conteudo do quinto post :)',
           datetime: '16/5/2023'
         }
-      ],
-      titleSearch: ''
+      ]
     }
   },
-  methods: {
-    submitForm(event) {
-      event.preventDefault()
-
-      let now = new Date()
-      let dataDaPostagem = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
-
-      this.posts.push({
-        title: this.formData.title,
-        content: this.formData.content,
-        datetime: dataDaPostagem
-      })
-
-      this.formData = {}
-    },
-  },
-  computed: {
-    filteredPost() {
-      /*       
-        const listaFinal = []
-        for (const post of this.posts) {
-          if (post.title.includes(this.titleSearch)) {
-            listaFinal.push(post)
-          }
-        } 
-      */
-
-      /* Forma mais reduzida
-      return this.posts.filter((item) => item.title.includes(this.titleSearch))
-      */
-
-      //Se caso o search estiver vazio, haverá retorno de todos os posts
-      // GUARD -> impede que o código seja executado sem que exista real necessidade
-      if (!this.titleSearch) return this.posts
-
-      return this.posts.filter((item) => {
-        return item.title.includes(this.titleSearch)
-      })
-
+  methods:{
+    addPost(objeto){
+      this.posts.push(objeto)
     }
   }
 }
@@ -82,108 +45,21 @@ export default {
 </script>
 
 <template>
-  <form id="form" action="">
-    <input v-model="formData.title" placeholder="Título">
-    <textarea v-model="formData.content" placeholder="Message"></textarea>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/create">Novo Post</RouterLink>
+  </header>
 
-    <button type="submit" @click="submitForm">Criar</button>
-  </form>
-
-
-  <div id="searchBox">
-    <input id="searchInput" type="text" v-model="titleSearch" placeholder="Procure pelo título do Post...">
-  </div>
-
-  <div id="lista-posts">
-    <div class="post center" v-for="x in filteredPost" :key="x.title">
-      <h3>{{ x.title }}</h3>
-      <h4>{{ x.datetime }}</h4>
-      <p>{{ x.content }}</p>
-    </div>
-  </div>
-
-  <!--   <RouterView /> -->
+  <!-- ESCUTA EVENTO E RECEBE OBJETO NA FUNÇÂO ADDPOST -->
+  <RouterView :posts="posts" @create-post="addPost"/>
 </template>
 
 <style>
-form {
+header{
+  width: 100vw;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  margin-bottom: 10px;
+  flex-direction: row;
+  justify-content: space-around;
 }
 
-#searchBox{
-  text-align: center;
-
-  border-top: 1px solid #cacaca;
-
-  padding-top: 10px;
-}
-
-form>*,
-#searchInput {
-  background-color: white;
-  box-shadow: 3px 3px 15px lightgray;
-  border: none;
-
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-
-  width: 500px;
-  padding: 10px;
-  margin-top: 5px;
-
-  border: 3px;
-}
-
-form button {
-  background-color: rgb(53, 53, 53);
-  color: whitesmoke;
-  font-weight: 800;
-  width: 100px;
-  border-radius: 10px;
-
-  transition: 0.5s;
-}
-
-form button:hover {
-  background-color: #2b1a1a;
-  box-shadow: 3px 3px 1px lightcoral;
-}
-
-
-#lista-posts {
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-
-  justify-content: center;
-
-  justify-content: center;
-}
-
-.post {
-  background-color: white;
-  height: 200px;
-  width: 270px;
-
-  box-shadow: 4px 4px 15px lightgray;
-
-  border-radius: 10px;
-  padding: 10px;
-
-  margin: 10px;
-}
-
-@media (max-width: 600px) {
-  form>*,
-  #searchInput {
-    width: 87vw;
-  }
-
-  .post{
-    width: 100vw;
-  }
-}
 </style>
