8ede3cdcd1f49e71b23b0036f834c927e54770ae
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-05-18 16:05:48 -0300

    desenvolvimento view de edição

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,3 +1,26 @@
+<script>
+
+import PostForm from '../components/PostForm.vue';
+
+export default {
+    props: {
+        posts: Array
+    },
+    components: {
+        PostForm
+    },
+    data() {
+        return {
+            post: this.posts[this.$route.params.id],
+        }
+    },
+}
+</script>
+
 <template>
-    Edit Page
+    <PostForm :post="post" />
+
+    {{ post }}
 </template>
+
+<style></style>
